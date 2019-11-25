9b273b405a7c72181a141c77b2e4e0c178f948e0
Peter Crowther <13117069+merrygoat@users.noreply.github.com>
2019-11-25 14:44:12 +0000

    Change sigma guess to percentage of data width

--- spectrum_fitting.py
+++ spectrum_fitting.py
@@ -11,6 +11,7 @@ import pandas as pd
 import dill
 
 import plotting
+from pv_fit import do_pv_fit
 
 
 class PeakParams:
@@ -78,73 +79,6 @@ class PeakFit:
             print("Cannot plot fit peak as fitting has not been done yet.")
         else:
             plotting.plot_peak_fit(self.raw_spectrum, self.cake_numbers, self.result, self.name)
-
-
-def do_pv_fit(peak_data: np.ndarray, maxima_locations: List[Tuple[float, float]],
-              fit_parameters: lmfit.Parameters = None):
-    """
-    Pseudo-Voigt fit to the lattice plane peak intensity.
-    Return results of the fit as an lmfit class, which contains the fitted parameters
-    (amplitude, fwhm, etc.) and the fit line calculated using the fit parameters and
-    100x two-theta points.
-    """
-    model = None
-    peak_prefix = "maximum_{}_"
-
-    num_maxima = len(maxima_locations)
-
-    for maxima_num in range(num_maxima):
-        # Add the peak to the model
-        if model:
-            model += lmfit.models.PseudoVoigtModel(prefix=peak_prefix.format(maxima_num + 1))
-        else:
-            model = lmfit.models.PseudoVoigtModel(prefix=peak_prefix.format(maxima_num + 1))
-    model += lmfit.Model(lambda background: background)
-
-    two_theta = peak_data[:, 0]
-    intensity = peak_data[:, 1]
-
-    if not fit_parameters:
-        fit_parameters = guess_params(two_theta, intensity, maxima_locations)
-
-    fit_result = model.fit(intensity, fit_parameters, x=two_theta,
-                           fit_kws={"xtol": 1e-7}, iter_cb=iteration_callback)
-
-    return fit_result
-
-
-def guess_params(x_data, y_data, maxima_ranges: List[Tuple[float, float]]) -> lmfit.Parameters:
-    """Given a dataset and some details about where the maxima are, guess some good initial
-    values for the PV fit."""
-    params = lmfit.Parameters()
-
-    for index, maximum in enumerate(maxima_ranges):
-        maximum_mask = np.logical_and(x_data > maximum[0],
-                                      x_data < maximum[1])
-        maxima_x = x_data[maximum_mask]
-        maxima_y = y_data[maximum_mask]
-        center = maxima_x[np.argmax(maxima_y)]
-
-        # Sigma is approximately 0.5 * FWHM.
-        sigma = 0.02
-        # Take the maximum height of the peak but the minimum height of the dataset overall
-        # This is because the maximum_mask does not necessarily include baseline points.
-        amplitude = ((max(maxima_y) - min(y_data)) * 3) * sigma
-
-        params.add(f"maximum_{index + 1}_center", value=center, min=maximum[0],
-                   max=maximum[1])
-        params.add(f"maximum_{index + 1}_sigma", value=sigma, min=0.01, max=0.02)
-        params.add(f"maximum_{index + 1}_fraction", value=0.2, min=0, max=1)
-        params.add(f"maximum_{index + 1}_amplitude", value=amplitude, min=0.05)
-    params.add("background", value=min(y_data), min=0, max=max(y_data))
-    return params
-
-
-# noinspection PyUnusedLocal
-def iteration_callback(parameters, iteration_num, residuals, *args, **kws):
-    """This method is called on every iteration of the minimisation. This can be used
-    to monitor progress."""
-    return False
 
 
 class FitSpectrum:
