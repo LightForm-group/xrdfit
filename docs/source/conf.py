bf5747c8c773c87d6d401a1c2837e79b47d3533e
Peter Crowther <13117069+merrygoat@users.noreply.github.com>
2019-12-16 19:05:16 +0000

    Add index to docs config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,6 +47,9 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
+# The master toctree document.
+master_doc = 'index'
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
