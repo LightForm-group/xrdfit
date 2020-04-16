2e36b33e84cc540cb7dd20572ffa4e66636c26fe
Peter Crowther <13117069+merrygoat@users.noreply.github.com>
2020-04-16 14:54:55 +0100

    Add mechanism to pass on a subset of fit params

--- xrdfit/pv_fit.py
+++ xrdfit/pv_fit.py
@@ -20,7 +20,7 @@ def do_pv_fit(peak_data: np.ndarray, peak_param: "PeakParams") -> lmfit.model.Mo
     :param peak_param: A PeakParams object describing the peak to be fitted.
     """
     model = None
-    fit_parameters = peak_param.previous_fit_parameters
+    old_fit_parameters = peak_param.previous_fit_parameters
     num_maxima = len(peak_param.maxima)
 
     # Add one peak to the model for each maximum
@@ -35,51 +35,67 @@ def do_pv_fit(peak_data: np.ndarray, peak_param: "PeakParams") -> lmfit.model.Mo
     two_theta = peak_data[:, 0]
     intensity = peak_data[:, 1]
 
-    if not fit_parameters:
-        fit_parameters = model.make_params()
-        fit_parameters = guess_params(fit_parameters, two_theta, intensity, peak_param.maxima)
-        # We can't use special characters in param names so have to save the user provided
-        # name in user_data.
-        for parameter in fit_parameters:
-            if parameter != "background":
-                parameter_num = int(parameter.split("_")[1])
-                fit_parameters[parameter].user_data = peak_param.maxima[parameter_num].name
+    # This generates the derived parameters as well as the fundamental parameters
 
-    fit_result = model.fit(intensity, fit_parameters, x=two_theta)
+    new_fit_parameters = guess_params(model, old_fit_parameters, two_theta, intensity,
+                                      peak_param.maxima)
+    # We can't use special characters in param names so have to save the user provided
+    # name in user_data.
+    for parameter in new_fit_parameters:
+        if parameter != "background":
+            parameter_num = int(parameter.split("_")[1])
+            new_fit_parameters[parameter].user_data = peak_param.maxima[parameter_num].name
+
+    fit_result = model.fit(intensity, new_fit_parameters, x=two_theta)
 
     return fit_result
 
 
-def guess_params(params: lmfit.Parameters, x_data: np.ndarray, y_data: np.ndarray,
+def guess_params(model: lmfit.Model, old_fit_params: lmfit.Parameters,
+                 x_data: np.ndarray, y_data: np.ndarray,
                  maxima_params: List["MaximumParams"]) -> lmfit.Parameters:
     """Given a dataset and some information about where the maxima are, guess some good initial
     values for the Pseudo-Voigt fit.
 
-    :param params: The lmfit.Parameters instance to store the guessed parameters.
+    :param model:  The lmfit Model to guess the params for.
+    :param old_fit_params: Any params that are to be passed on from a previous fit
     :param x_data: The x data to be fitted.
     :param y_data: The y data to be fitted.
     :param maxima_params: The MaximaParams specified by the user.
     """
+    new_fit_parameters = model.make_params()
     for index, maximum in enumerate(maxima_params):
-        maximum_mask = np.logical_and(x_data > maximum.bounds[0], x_data < maximum.bounds[1])
-        maxima_x = x_data[maximum_mask]
-        maxima_y = y_data[maximum_mask]
-        center = maxima_x[np.argmax(maxima_y)]
+        prefix = f"maximum_{index}"
+        # If the params have been passed on then use them
+        if old_fit_params and f"{prefix}_center" in old_fit_params:
+            new_fit_parameters[f"{prefix}_center"] = old_fit_params[f"{prefix}_center"]
+            new_fit_parameters[f"{prefix}_sigma"] = old_fit_params[f"{prefix}_sigma"]
+            new_fit_parameters[f"{prefix}_fraction"] = old_fit_params[f"{prefix}_fraction"]
+            new_fit_parameters[f"{prefix}_amplitude"] = old_fit_params[f"{prefix}_amplitude"]
+        # If params haven't been passed on then guess new ones
+        else:
+            maximum_mask = np.logical_and(x_data > maximum.bounds[0], x_data < maximum.bounds[1])
+            maxima_x = x_data[maximum_mask]
+            maxima_y = y_data[maximum_mask]
+            center = maxima_x[np.argmax(maxima_y)]
 
-        max_sigma, min_sigma, sigma = guess_sigma(x_data, maximum.bounds)
-        # When calculating amplitude take the maximum height of the peak but the minimum height
-        # of the dataset overall. This is because the maximum_mask does not necessarily
-        # include baseline points and we need the noise level.
-        amplitude = (max(maxima_y) - min(y_data)) * 2 * sigma
-        param_prefix = f"maximum_{index}"
-        params.add(f"{param_prefix}_center", value=center, min=maximum.bounds[0],
-                   max=maximum.bounds[1])
-        params.add(f"{param_prefix}_sigma", value=sigma, min=min_sigma, max=max_sigma)
-        params.add(f"{param_prefix}_fraction", value=0.2, min=0, max=1)
-        params.add(f"{param_prefix}_amplitude", value=amplitude, min=0)
-    # Background should be > 0, but a little flexibility here improves the convergence of the fit.
-    params.add("background", value=min(y_data), min=-10, max=max(y_data))
-    return params
+            max_sigma, min_sigma, sigma = guess_sigma(x_data, maximum.bounds)
+            # When calculating amplitude take the maximum height of the peak but the minimum height
+            # of the dataset overall. This is because the maximum_mask does not necessarily
+            # include baseline points and we need the noise level.
+            amplitude = (max(maxima_y) - min(y_data)) * 2 * sigma
+            new_fit_parameters.add(f"{prefix}_center", value=center, min=maximum.bounds[0],
+                                   max=maximum.bounds[1])
+            new_fit_parameters.add(f"{prefix}_sigma", value=sigma, min=min_sigma, max=max_sigma)
+            new_fit_parameters.add(f"{prefix}_fraction", value=0.2, min=0, max=1)
+            new_fit_parameters.add(f"{prefix}_amplitude", value=amplitude, min=0)
+
+    if old_fit_params and "background" in old_fit_params:
+        new_fit_parameters["background"] = old_fit_params["background"]
+    else:
+        # Background should be > 0, but a little flexibility here improves fit convergence.
+        new_fit_parameters.add("background", value=min(y_data), min=-10, max=max(y_data))
+    return new_fit_parameters
 
 
 def guess_sigma(x_data: np.ndarray,
